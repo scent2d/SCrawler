e8ad1b0e570debc5929f90ded9972841251e60aa
scent2d <45614318+scent2d@users.noreply.github.com>
2021-03-08 21:22:10 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,123 @@
+// go run main.go [target]
+// go run main.go https://youtube.com
+// go run main.go https://youtube.com > output.txt
+
+package main
+
+import (
+	_ "bufio"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/steelx/extractlinks"
+)
+
+var (
+	config = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	transport = &http.Transport{
+		TLSClientConfig: config,
+	}
+	netClient = &http.Client{
+		Transport: transport,
+	}
+	queue      = make(chan string)
+	hasVisited = make(map[string]bool)
+)
+
+// 크롤링 함수
+func crawlURL(href string) {
+	hasVisited[href] = true
+	fmt.Printf("Crawling URL --> %v \n", href)
+
+	// 결과 파일로 쓰기
+	// file, err := os.OpenFile("/Users/scent2d/golang/src/SCrawler2/output.txt", os.O_CREATE|os.O_RDWR, os.FileMode(0777))
+	// checkError(err)
+
+	// defer file.Close()
+
+	// w := bufio.NewWriter(file)
+	// w.WriteString(href + "\r\n")
+	// w.Flush()
+
+	response, err := netClient.Get(href)
+	checkError(err)
+	defer response.Body.Close()
+
+	links, err := extractlinks.All(response.Body)
+	checkError(err)
+
+	for _, link := range links {
+		absoluteURL := toFixedURL(link.Href, href)
+		go func() {
+			queue <- absoluteURL
+		}()
+	}
+}
+
+// 도메인 비교함수
+func isSameDomain(href string, baseURL string) bool {
+	url, err := url.Parse(href)
+	if err != nil {
+		return false
+	}
+	parentURL, err := url.Parse(baseURL)
+	if err != nil {
+		return false
+	}
+	if url.Host != parentURL.Host {
+		return false
+	}
+
+	return true
+}
+
+// URL 통일함수
+func toFixedURL(href string, baseURL string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+
+	base, err := url.Parse(baseURL)
+	if err != nil {
+		return ""
+	}
+
+	fixedURL := base.ResolveReference(uri)
+
+	return fixedURL.String()
+}
+
+// 예외처리 함수
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	// 실행 어규먼츠 배열 선언
+	arguments := os.Args[1:]
+	if len(arguments) == 0 {
+		fmt.Println("Arguments Missing !")
+		os.Exit(1)
+	}
+
+	baseURL := arguments[0]
+
+	go func() {
+		queue <- baseURL
+	}()
+
+	for href := range queue {
+		if !hasVisited[href] && isSameDomain(href, baseURL) {
+			crawlURL(href)
+		}
+	}
+}
